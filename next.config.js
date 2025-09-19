48d4fd99cd9d8d789261b7bcd955d293a51aba46
benjaminpeto <benjaminpeto23@gmail.com>
2025-09-19 11:59:40 +0100

    update enxt config

--- next.config.js
+++ next.config.js
@@ -9,12 +9,8 @@ const nextConfig = {
                         key: "Content-Security-Policy",
                         // keep on one line in prod; this is an example policy
                         value:
-                            "default-src 'self'; " +
                             "script-src 'self' https://locationdb.com https://preview.locationdb.com; " +
-                            "frame-src https://locationdb.com https://preview.locationdb.com; " +
-                            "img-src 'self' data: https:; " +
-                            "style-src 'self' 'unsafe-inline'; " +
-                            "connect-src 'self';",
+                            "frame-src https://locationdb.com https://preview.locationdb.com; ",
                     },
                 ],
             },
