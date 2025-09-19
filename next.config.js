76569dfdcaa4dfa0ae16fd92d2b3dcff4b9eeaca
benjaminpeto <benjaminpeto23@gmail.com>
2025-09-19 11:51:09 +0100

    update next.config with headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "Content-Security-Policy",
+                        // keep on one line in prod; this is an example policy
+                        value:
+                            "default-src 'self'; " +
+                            "script-src 'self' https://locationdb.com https://preview.locationdb.com; " +
+                            "frame-src https://locationdb.com https://preview.locationdb.com; " +
+                            "img-src 'self' data: https:; " +
+                            "style-src 'self' 'unsafe-inline'; " +
+                            "connect-src 'self';",
+                    },
+                ],
+            },
+        ];
+    },
+};
 
 module.exports = nextConfig;
